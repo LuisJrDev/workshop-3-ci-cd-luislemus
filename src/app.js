c7b20ffd4d005abb746850aacdb283133226202e
Luis Jr. Lemus <110882020+LuisJrDev@users.noreply.github.com>
2024-11-14 20:25:52 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 // Middleware para parsear JSON
 app.use(express.json());
 app.get('/', (req, res) => {
- res.status(200).send('Bienvenido a la API');
+ res.statu(200).send('Bienvenido a la API');
 });
 
 app.get('/status', (req, res) => {
